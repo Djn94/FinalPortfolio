8589c19c8f710dc0fcb47dc1ad9d7adabb97ee79
Non Dewman <Dnewman1934@yahoo.com>
2019-07-02 21:48:13 -0500

    Updates

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-items');
+const ghpages = require('gh-pages');
+ghpages.publish('dist', callback);
+
 
 let showMenu = false;
 menuBtn.addEventListener('click', toggleMenu);
